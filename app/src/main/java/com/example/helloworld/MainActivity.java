72da3a4c2df431c577eeb736adc13099467dfb69
ÁngelDavidArellanoM <angeldamc11@gmail.com>
2023-09-28 14:14:40 +0200

    perro

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText ageEdit = findViewById(R.id.age_edit);
+        TextView resultText = findViewById(R.id.result_set);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String edad = ageEdit.getText().toString();
+                if (!edad.isEmpty()){
+                    int ageInt = Integer.parseInt(edad);
+                    int result = ageInt * 7;
+                    String resultString = ("Si fueras perro, te edad sería de " + result + " años");
+
+                    resultText.setText(resultString);
+                }
+
+            }
+        });
     }
 }
